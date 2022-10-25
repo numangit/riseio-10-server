09f1ee54dbeef3275fe9c07f7493b4c1b8cb4bc7
numangit <abdulnumanjob@gmail.com>
2022-10-25 21:34:54 +0600

    course catagories api created

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Course API server running');
+});
+
+app.get('/all-courses', (req, res) => {
+    res.send(courses);
 });
 
 app.listen(port, () => {
